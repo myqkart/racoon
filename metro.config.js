3bb2ac38d76e22e7ae4813e7cc7652bbe381e3aa
Kamlesh Mundel <kamlesh@techuz.com>
2023-06-05 23:04:01 +0530

    metro config file

--- metro.config.js
+++ metro.config.js
@@ -1,25 +1,23 @@
-const { getDefaultConfig } = require("metro-config");
+const { getDefaultConfig } = require("@expo/metro-config");
 
-module.exports = (async () => { 
-    const {  
-        resolver: { 
-            sourceExts, 
-            assetExts 
-        }  
-    } = await getDefaultConfig(); 
+module.exports = (async () => {
+  const defaultConfig = await getDefaultConfig(__dirname);
 
-    return {
-        transformer: {      
-            babelTransformerPath: require.resolve("react-native-svg-transformer"),
-            getTransformOptions: async () => ({
-              transform: {
-                experimentalImportSupport: false,
-                inlineRequires: true,
-              },
-            }),
+  return {
+    ...defaultConfig,
+    transformer: {
+      ...defaultConfig.transformer,
+      babelTransformerPath: require.resolve("react-native-svg-transformer"),
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
         },
-        resolver: {
-            assetExts: assetExts.filter(ext => ext !== "svg"),
-            sourceExts: [...sourceExts, "svg"]    
-        }};
+      }),
+    },
+    resolver: {
+      ...defaultConfig.resolver,
+      assetExts: [...defaultConfig.resolver.assetExts, "svg"],
+    },
+  };
 })();
